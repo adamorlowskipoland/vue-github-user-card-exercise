f7e1c270525a4c62c2e40eaceec3e02de8094d94
Adam <adamorlowskipoland@icloud.com>
2019-04-05 12:29:03 +0200

    make component functional

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,13 +1,13 @@
-<template>
+<template functional>
   <div
     class="user-card mx-auto max-w-xs p-2 bg-grey-lighter max-w-xs rounded overflow-hidden shadow-lg text-center"
   >
-    <img :src="imgUrl" :alt="imgAlt" class="w-full">
+    <img :src="props.imgUrl" :alt="props.imgAlt" class="w-full">
     <div class="px-6 py-4">
-      <div class="font-bold text-xl mt-2">{{name}}</div>
-      <p class="text-grey-darker text-xs mt-2">Joined in {{created}}</p>
-      <p class="text-grey-darker text-xs mt-2">{{description}}</p>
-      <p class="text-grey-darker text-xs mt-2">Location: {{location}}</p>
+      <div class="font-bold text-xl mt-2">{{props.name}}</div>
+      <p class="text-grey-darker text-xs mt-2">Joined in {{props.created}}</p>
+      <p class="text-grey-darker text-xs mt-2">{{props.description}}</p>
+      <p class="text-grey-darker text-xs mt-2">Location: {{props.location}}</p>
     </div>
     <span
       class="inline-block bg-teal-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker"
@@ -22,7 +22,7 @@
           d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"
         ></path>
       </svg>
-      {{followers}} {{followers.length > 1 ? 'follower' : 'followers'}}
+      {{followers}} {{props.followers.length > 1 ? 'follower' : 'followers'}}
     </span>
   </div>
 </template>
